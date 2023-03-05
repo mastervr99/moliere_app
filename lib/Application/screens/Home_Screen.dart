bccfa9a23b92c3ef077c0037b4c647932f4aa6fe
thusy <cleverage6@gmail.com>
2023-03-05 16:04:45 +0100

    Opening pdf ok

--- lib/Application/screens/Home_Screen.dart
+++ lib/Application/screens/Home_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:moliere_app/Application/common_widgets/Appbar_Widget.dart';
 import 'package:moliere_app/Application/common_widgets/Circular_Progress_Widget.dart';
 import 'package:moliere_app/Application/common_widgets/Search_Widget.dart';
@@ -10,94 +11,176 @@ class Home_Screen extends StatefulWidget {
   _Home_Screen_State createState() => _Home_Screen_State();
 }
 
-get_doc() async {
-  PDFDocument doc =
-      await PDFDocument.fromAsset('assets/ebooks/Les_Amants_magnifiques.pdf');
-  // return await doc;
+// get_doc() async {
+//   PDFDocument doc =
+//       await PDFDocument.fromAsset('assets/ebooks/Les_Amants_magnifiques.pdf');
+//   // return await doc;
 
-  return FutureBuilder(
-    future: PDFDocument.fromAsset('assets/ebooks/Les_Amants_magnifiques.pdf'),
-    builder: (context, AsyncSnapshot snapshot) {
-      var doc = snapshot.data;
+//   return FutureBuilder(
+//     future: PDFDocument.fromAsset('assets/ebooks/Les_Amants_magnifiques.pdf'),
+//     builder: (context, AsyncSnapshot snapshot) {
+//       // var doc = snapshot.data;
+//       var doc = true;
 
-      if (snapshot.hasData) {
-        return AlertDialog(
-          content: Text("data"),
-        );
-        // return SizedBox(
-        //   height: MediaQuery.of(context).size.height,
-        //   width: MediaQuery.of(context).size.width,
-        //   child: Center(
-        //     child: PDFViewer(document: doc),
-        //   ),
-        // );
-      } else if (snapshot.hasError) {
-        return AlertDialog(
-          content: Text("error"),
-        );
+//       if (doc) {
+//         return AlertDialog(
+//           content: Text("data"),
+//         );
+//         // return SizedBox(
+//         //   height: MediaQuery.of(context).size.height,
+//         //   width: MediaQuery.of(context).size.width,
+//         //   child: Center(
+//         //     child: PDFViewer(document: doc),
+//         //   ),
+//         // );
+//         // } else if (snapshot.hasError) {
+//         //   return AlertDialog(
+//         //     content: Text("error"),
+//         //   );
 
-        // return Text('Error: ${snapshot.error}');
-      } else {
-        return Circular_Progress_Widget();
-      }
-    },
-  );
-}
+//         //   // return Text('Error: ${snapshot.error}');
+//       } else {
+//         return Circular_Progress_Widget();
+//       }
+//     },
+//   );
+// }
 
 class _Home_Screen_State extends State<Home_Screen> {
+  void loadDocument(BuildContext context) async {
+    PDFDocument document =
+        await PDFDocument.fromAsset('assets/ebooks/Les_Amants_magnifiques.pdf');
+    // Open the PDF viewer page
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PDFViewerPage(document: document),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: Appbar_Widget(context),
+    //   body: Container(
+    //       width: double.infinity,
+    //       height: double.infinity,
+    //       color: Colors.white70,
+    //       child: Column(
+    //         children: [
+    //           //Search_Widget(),
+    //           Expanded(
+    //             child: ListView(
+    //               children: [
+    //                 // Text("TEST"),
+    //                 // ElevatedButton(
+    //                 //   child: Text(('Book')),
+    //                 //   onPressed: () async {
+    //                 //     return get_doc();
+    //                 //     // FutureBuilder(
+    //                 //     //   future: get_doc(),
+    //                 //     //   builder: (context, AsyncSnapshot snapshot) {
+    //                 //     //     var doc = snapshot.data;
+
+    //                 //     //     if (snapshot.hasData) {
+    //                 //     //       return AlertDialog(
+    //                 //     //         content: Text("data"),
+    //                 //     //       );
+    //                 //     //       // return SizedBox(
+    //                 //     //       //   height: MediaQuery.of(context).size.height,
+    //                 //     //       //   width: MediaQuery.of(context).size.width,
+    //                 //     //       //   child: Center(
+    //                 //     //       //     child: PDFViewer(document: doc),
+    //                 //     //       //   ),
+    //                 //     //       // );
+    //                 //     //     } else if (snapshot.hasError) {
+    //                 //     //       return AlertDialog(
+    //                 //     //         content: Text("error"),
+    //                 //     //       );
+
+    //                 //     //       // return Text('Error: ${snapshot.error}');
+    //                 //     //     } else {
+    //                 //     //       return Circular_Progress_Widget();
+    //                 //     //     }
+    //                 //     //   },
+    //                 //     // );
+    //                 //   },
+    //                 // ),
+    //                 ElevatedButton(
+    //                   onPressed: () {
+    //                     Navigator.push(
+    //                       context,
+    //                       MaterialPageRoute(
+    //                         builder: (context) => PdfViewPage(
+    //                           pdfPath:
+    //                               'assets/ebooks/Les_Amants_magnifiques.pdf',
+    //                         ),
+    //                       ),
+    //                     );
+    //                   },
+    //                   child: Text('Open PDF'),
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         ],
+    //       )),
+    // );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Home Page'),
+    //   ),
+    //   body: Center(
+    //     child: ElevatedButton(
+    //       onPressed: () {
+    //         Navigator.push(
+    //           context,
+    //           MaterialPageRoute(
+    //             builder: (context) => PdfViewPage(
+    //               pdfPath: 'assets/ebooks/Les_Amants_magnifiques.pdf',
+    //             ),
+    //           ),
+    //         );
+    //       },
+    //       child: Text('Open PDF'),
+    //     ),
+    //   ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PDF Viewer Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            loadDocument(context);
+          },
+          child: Text('Open PDF'),
+        ),
+      ),
+    );
+  }
+}
+
+class PDFViewerPage extends StatelessWidget {
+  final PDFDocument document;
+
+  PDFViewerPage({required this.document});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Appbar_Widget(context),
-      body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.white70,
-          child: Column(
-            children: [
-              //Search_Widget(),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Text("TEST"),
-                    ElevatedButton(
-                      child: Text(('Book')),
-                      onPressed: () async {
-                        return get_doc();
-                        // FutureBuilder(
-                        //   future: get_doc(),
-                        //   builder: (context, AsyncSnapshot snapshot) {
-                        //     var doc = snapshot.data;
-
-                        //     if (snapshot.hasData) {
-                        //       return AlertDialog(
-                        //         content: Text("data"),
-                        //       );
-                        //       // return SizedBox(
-                        //       //   height: MediaQuery.of(context).size.height,
-                        //       //   width: MediaQuery.of(context).size.width,
-                        //       //   child: Center(
-                        //       //     child: PDFViewer(document: doc),
-                        //       //   ),
-                        //       // );
-                        //     } else if (snapshot.hasError) {
-                        //       return AlertDialog(
-                        //         content: Text("error"),
-                        //       );
-
-                        //       // return Text('Error: ${snapshot.error}');
-                        //     } else {
-                        //       return Circular_Progress_Widget();
-                        //     }
-                        //   },
-                        // );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )),
+      appBar: AppBar(
+        title: Text('PDF Viewer'),
+      ),
+      body: Center(
+        child: PDFViewer(
+          document: document,
+          scrollDirection: Axis.vertical,
+          lazyLoad: false,
+        ),
+      ),
     );
   }
 }
